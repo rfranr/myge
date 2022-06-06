78b1e7bb41d024275adb7ff7b397935e6607199f
rfranr <rfranr@gmail.com>
2022-06-06 10:46:11 +0200

    use Clist library

--- source/core/ecs/ecs.c
+++ source/core/ecs/ecs.c
@@ -1,21 +1,43 @@
 #include "ecs.h";
+#include <clist.h>
 
-struct ecs
-{
-    /* data */
-    long id;
-    long version;
-    long size;
-    long capacity;
-    long *components;
-    long *components_size;
-    long *components_capacity;
-    long *components_count;
-    long *components_type;
-};
 
+void test_list(){
+    int list_size;
+    long l;
+    struct ecs_t *element_at;
+
+    CList *list = CList_init(sizeof(ecs_t));
+
+
+    for (l=0; l<1000;l++){
+
+        struct ecs_t ecs;
+        {
+            ecs.id = l;
+        };
+
+
+        list->add(list, &ecs);
+    }   
+
+
+    list_size = list->count(list);
+    printf("list_size: %d\n", list_size);
+
+    element_at = list->at(list, 500);
+
+    printf("list_at: %d\n", element_at->id);
+
+
+    list->free(list);
+
+}
 
 long init_ecs(){
+
+    test_list();
+
     return 0;
 }
 
--- source/core/ecs/ecs.h
+++ source/core/ecs/ecs.h
@@ -2,6 +2,20 @@
  * Entity Component System
 */
 
+struct ecs_t
+{
+    /* data */
+    long id;
+    long version;
+    long size;
+    long capacity;
+    long *components;
+    long *components_size;
+    long *components_capacity;
+    long *components_count;
+    long *components_type;
+} ecs_t;
+
 long init_ecs();
 
 long add_entity(int eid);
